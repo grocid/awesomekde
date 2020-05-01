b9096c386f7a112d01e26e94d884dc4249d2fe17
grocid <carl@grocid.net>
2020-05-01 10:29:32 +0200

    add SSH rules

---
+++ setup-ufw.sh
@@ -0,0 +1,10 @@
+
+PORT=22
+
+echo "Setting up UFW rules for port $PORT and for $(hostname -I)"
+
+for IP in $(hostname -I)
+do
+	IPMASK=$(echo $IP | awk -F"." '{print $1"."$2"."$3".0/24"}')
+	sudo ufw allow from $IPMASK to any port $PORT
+done
