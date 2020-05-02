4c759fb7bb2b7e52e2f5be33bca3bc2168ae3d07
grocid <carl@grocid.net>
2020-05-02 20:02:21 +0200

    more fixes

--- wallpapers/generate_markdown.py
+++ wallpapers/generate_markdown.py
@@ -9,10 +9,10 @@ from os import listdir
 from os.path import isfile, join
 
 
-with open("WALLPAPERS.md", "w+") as fo:
-	fo.write(
-		base + " | ".join("<img width=\"%d\" src=\"%s\">" % 
-		(SIZE, f) + "| \n" * 1 * (i % 3 == 0) for i, f in enumerate(listdir(".")) if isfile(join(f)))
-	)
+#with open("WALLPAPERS.md", "w+") as fo:
+print(
+	base + " | ".join("<img width=\"%d\" src=\"%s\">" % 
+	(SIZE, f) + "| \n" * 1 * (i % 3 == 0) for i, f in enumerate(listdir(".")) if isfile(join(f)))
+)
 
 
