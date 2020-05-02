799545041f081219d104e077162f919e08f4217f
grocid <carl@grocid.net>
2020-05-02 18:25:28 +0200

    more fix

--- wallpapers/generate_markdown.py
+++ wallpapers/generate_markdown.py
@@ -10,9 +10,9 @@ from os.path import isfile, join
 
 
 with open("WALLPAPERS.md", "w+") as fo:
-	fo.write(
+	print(
 		base + " | ".join("<img width=\"%d\" src=\"%s\">" % 
-		(SIZE, f) for f in listdir(".") if isfile(join(f)))
+		(SIZE, f) + "| \n" * 1 * (i % 3 == 0) for i, f in enumerate(listdir(".")) if isfile(join(f)))
 	)
 
 
